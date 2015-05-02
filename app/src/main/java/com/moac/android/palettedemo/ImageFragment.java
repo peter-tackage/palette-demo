6a368d5a741363ba237d8262a2b872136e89b9cd
Peter Tackage <peter.tackage@gmail.com>
2015-05-02 12:50:05 +0200

    Save and restore PaletteOption on configuration change

--- app/src/main/java/com/moac/android/palettedemo/ImageFragment.java
+++ app/src/main/java/com/moac/android/palettedemo/ImageFragment.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.graphics.Palette;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +18,20 @@ import java.util.Observer;
 
 public class ImageFragment extends Fragment implements Observer {
 
+    private static final String TAG = ImageFragment.class.getSimpleName();
+
     private static final String RESOURCE_ID_ARG = "RESOURCE_ID_ARG";
-    private static final String PALETTE_OPTION_ARG = "PALETTE_OPTION_ARG";
+    private static final String KEY_PALETTE_OPTION = "PALETTE_OPTION";
 
     private ImageView imageView;
     private FragmentContainer fragmentContainer;
+    private PaletteOption paletteOption;
 
     public static ImageFragment create(int resourceId, PaletteOption option) {
         ImageFragment fragment = new ImageFragment();
         Bundle args = new Bundle();
         args.putInt(RESOURCE_ID_ARG, resourceId);
-        args.putSerializable(PALETTE_OPTION_ARG, option);
+        args.putSerializable(KEY_PALETTE_OPTION, option);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,8 +66,13 @@ public class ImageFragment extends Fragment implements Observer {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        PaletteOption initialOption = (PaletteOption) getArguments().getSerializable(PALETTE_OPTION_ARG);
-        onChangePaletteOption(initialOption);
+        PaletteOption initialPaletteOption;
+        if (savedInstanceState == null) {
+            initialPaletteOption = (PaletteOption) getArguments().getSerializable(KEY_PALETTE_OPTION);
+        } else {
+            initialPaletteOption = (PaletteOption) savedInstanceState.getSerializable(KEY_PALETTE_OPTION);
+        }
+        onChangePaletteOption(initialPaletteOption);
     }
 
     @Override
@@ -78,12 +87,21 @@ public class ImageFragment extends Fragment implements Observer {
         getPaletteObservable().deleteObserver(this);
     }
 
+    @Override public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(KEY_PALETTE_OPTION, paletteOption);
+    }
+
     @Override
     public void update(Observable observable, Object data) {
         onChangePaletteOption((PaletteOption) data);
     }
 
     private void onChangePaletteOption(PaletteOption paletteOption) {
+
+        Log.d(TAG, "onChangePaletteOption(): " + paletteOption);
+
+        this.paletteOption = paletteOption;
 
         if (getView() == null) return;
 
